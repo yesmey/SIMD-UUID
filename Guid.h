efe9cded0c1015c1547febaa9fc0740342997baf
Jesper Meyer <jesper.meyer@sogeti.se>
2016-08-23 14:11:13 +0200

    Add complete error checking (at the cost of ~20% worse performance)

--- Guid.h
+++ Guid.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 
-#include <smmintrin.h>
+#include <nmmintrin.h>
 
 class Guid
 {
@@ -21,17 +21,26 @@ public:
         __declspec(align(16)) const char y[17] = { chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr };
         return _mm_load_si128(reinterpret_cast<const __m128i*>(&y));
     }
+    
+    static __m128i loadAllowedCharRange()
+    {
+        __declspec(align(16)) const char _charRanges[17] = { '0', '9', 'A', 'Z', 'a', 'z', 0, -1, 0, -1, 0, -1, 0, -1, 0, -1 };
+        return _mm_load_si128(reinterpret_cast<const __m128i*>(&_charRanges));
+    }
 
     int Parse(const char* ptr)
     {
-        const __m128i zero = loadSimdCharArray('0');
-
         const __m128i str = _mm_loadu_si128(reinterpret_cast<const __m128i *>(ptr));
         const __m128i str2 = _mm_loadu_si128(reinterpret_cast<const __m128i *>(ptr + 19));
 
-        // 8%
-        if (_mm_movemask_epi8(_mm_cmplt_epi8(str, zero)) != 8448 ||
-            _mm_movemask_epi8(_mm_cmplt_epi8(str2, zero)) != 16)
+        const __m128i allowedCharRange = loadAllowedCharRange();
+
+        auto cmp = _mm_cmpistrm(allowedCharRange, str, _SIDD_UBYTE_OPS | _SIDD_CMP_RANGES | _SIDD_NEGATIVE_POLARITY);
+        auto cmp2 = _mm_cmpistrm(allowedCharRange, str2, _SIDD_UBYTE_OPS | _SIDD_CMP_RANGES | _SIDD_NEGATIVE_POLARITY);
+
+        // Checks if all chars (except all the expected '-') are between: 0-9, A-Z and a-z)
+        if (!_mm_testc_si128(_mm_cmpeq_epi64(cmp, _mm_set_epi64x(0LL, 8448LL)), _mm_set1_epi64x(-1)) ||
+            !_mm_testc_si128(_mm_cmpeq_epi64(cmp2, _mm_set_epi64x(0LL, 16LL)), _mm_set1_epi64x(-1)))
             return -1;
 
         const __m128i MASK_1 = _mm_setr_epi8(
@@ -91,6 +100,7 @@ public:
         mask2 = _mm_blendv_epi8(mask2, fixedUppercase2, aboveNineMask2);
 
         // 12%
+        const __m128i zero = loadSimdCharArray('0');
         __m128i hi = _mm_sub_epi8(mask1, zero);
         hi = _mm_slli_epi16(hi, 4);
         const __m128i lo = _mm_sub_epi8(mask2, zero);
