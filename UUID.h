3b761a4bfb1047de738e5e8fea09b7da0588f40c
Jesper Meyer <jesper.meyer@sogeti.se>
2016-11-22 16:01:59 +0100

    Fixed to_string. Removed the generator

--- UUID.h
+++ UUID.h
@@ -1,13 +1,13 @@
 #pragma once
+
 #include <cstdint>
 #include <string>
+#include <array>
 #include <nmmintrin.h>
 
 #if defined(_MSC_VER)
 #define ALIGNED_(size) __declspec(align(size))
 #define NO_INLINE __declspec(noinline)
-#include <Windows.h>
-#include <Wincrypt.h>
 #else
 #define ALIGNED_(size) __attribute__((__aligned__(size)))
 #define NO_INLINE __attribute__ ((noinline))
@@ -17,7 +17,6 @@ namespace meyr {
     class UUID final
     {
     public:
-
         enum BracketType
         {
             BRACKET_NONE = 0,
@@ -39,6 +38,7 @@ namespace meyr {
         };
 
         UUID() = default;
+        ~UUID() = default;
 
         UUID(std::string const& str)
         {
@@ -47,15 +47,13 @@ namespace meyr {
 
         UUID(UUID const& val)
         {
-            _mm_store_si128(reinterpret_cast<__m128i *>(_uuid.data), _mm_load_si128(reinterpret_cast<const __m128i *>(&val._uuid.data[0])));
+            _mm_store_si128(reinterpret_cast<__m128i *>(_data), _mm_load_si128(reinterpret_cast<const __m128i *>(&val._data[0])));
         }
-
-        ~UUID() = default;
 
         template<BracketType bracket = BRACKET_NONE, HexCaseType hexCase = HEX_CASE_NONE, ValidationType validation = VALIDATE_ALL>
         int Parse(std::string const& str)
         {
-            auto ptr = str.c_str();
+            const char* ptr = str.c_str();
             if (bracket == BRACKET_BRACES)
             {
                 // Expect the string string to start and end with bracers (no trailing whitespace allowed)
@@ -78,13 +76,13 @@ namespace meyr {
 
         unsigned char const* Data() const
         {
-            return &_uuid.data[0];
+            return &_data[0];
         }
 
         bool operator ==(UUID const& other) const noexcept
         {
-            __m128i mm_left = _mm_load_si128(reinterpret_cast<const __m128i*>(_uuid.data));
-            __m128i mm_right = _mm_load_si128(reinterpret_cast<const __m128i*>(other._uuid.data));
+            __m128i mm_left = _mm_load_si128(reinterpret_cast<const __m128i*>(_data));
+            __m128i mm_right = _mm_load_si128(reinterpret_cast<const __m128i*>(other._data));
             __m128i eq = _mm_cmpeq_epi32(mm_left, mm_right);
             return _mm_testc_si128(eq, _mm_cmpeq_epi32(eq, eq)) != 0;
         }
@@ -96,8 +94,8 @@ namespace meyr {
 
         bool operator <(UUID const& other) const noexcept
         {
-            __m128i mm_left = _mm_load_si128(reinterpret_cast<const __m128i*>(_uuid.data));
-            __m128i mm_right = _mm_load_si128(reinterpret_cast<const __m128i*>(other._uuid.data));
+            __m128i mm_left = _mm_load_si128(reinterpret_cast<const __m128i*>(_data));
+            __m128i mm_right = _mm_load_si128(reinterpret_cast<const __m128i*>(other._data));
 
             const __m128i mm_signs_mask = _mm_xor_si128(mm_left, mm_right);
             __m128i mm_cmp = _mm_cmpgt_epi8(mm_right, mm_left);
@@ -105,10 +103,10 @@ namespace meyr {
             mm_cmp = _mm_xor_si128(mm_signs_mask, mm_cmp);
             mm_rcmp = _mm_xor_si128(mm_signs_mask, mm_rcmp);
 
-            uint32_t cmp = static_cast<uint32_t>(_mm_movemask_epi8(mm_cmp));
-            uint32_t rcmp = static_cast<uint32_t>(_mm_movemask_epi8(mm_rcmp));
-            cmp = (cmp - 1u) ^ cmp;
-            rcmp = (rcmp - 1u) ^ rcmp;
+            int cmp = _mm_movemask_epi8(mm_cmp);
+            int rcmp = _mm_movemask_epi8(mm_rcmp);
+            cmp = (cmp - 1) ^ cmp;
+            rcmp = (rcmp - 1) ^ rcmp;
             return cmp < rcmp;
         }
 
@@ -127,39 +125,10 @@ namespace meyr {
             return !(*this < rhs);
         }
 
-        /*std::string to_string() const
+        std::array<char, 36> to_string() const
         {
-            std::string result;
-            result.reserve(36);
-
-            for (int i = 0; i < 16; ++i) {
-                const size_t hi = (_uuid.data[i] >> 4) & 0x0F;
-                if (hi <= 9) {
-                    result.push_back(static_cast<char>('0' + hi));
-                }
-                else {
-                    result.push_back(static_cast<char>('a' + (hi - 10)));
-                }
-
-                const size_t lo = _uuid.data[i] & 0x0F;
-                if (lo <= 9) {
-                    result.push_back(static_cast<char>('0' + lo));
-                }
-                else {
-                    result.push_back(static_cast<char>('a' + (lo - 10)));
-                }
-
-                if (i == 3 || i == 5 || i == 7 || i == 9) {
-                    result.push_back('-');
-                }
-            }
-            return result;
-        }*/
-
-        std::string to_string() const
-        {
-            const auto a = _mm_set1_epi8(0x0F);
-            __m128i lower = _mm_load_si128(reinterpret_cast<const __m128i*>(_uuid.data));
+            const __m128i a = _mm_set1_epi8(0x0F);
+            __m128i lower = _mm_load_si128(reinterpret_cast<const __m128i*>(_data));
             __m128i upper = _mm_and_si128(_mm_set1_epi8(0xFF >> 4), _mm_srli_epi32(lower, 4));
 
             lower = _mm_and_si128(lower, a);
@@ -175,8 +144,8 @@ namespace meyr {
             __m128i letterMask3 = _mm_or_si128(lower, lowerMask);
             __m128i letterMask4 = _mm_or_si128(upper, upperMask);
 
-            const auto first = _mm_set1_epi8('0');
-            const auto second = _mm_set1_epi8('a' - 10);
+            const __m128i first = _mm_set1_epi8('0');
+            const __m128i second = _mm_set1_epi8('a' - 10);
 
             letterMask1 = _mm_add_epi8(letterMask1, first);
             letterMask2 = _mm_add_epi8(letterMask2, first);
@@ -200,16 +169,16 @@ namespace meyr {
                 -1, -1, 6, -1);
 
             const __m128i MASK_3 = _mm_setr_epi8(
-                -1, 7, -1, -1, 8,
-                -1, 9, -1, -1,
-                10, -1, 11, -1, 12,
-                -1, 13);
+                -1, 7, -1, -1,
+                8, -1, 9, -1,
+                -1, 10, -1, 11,
+                -1, 12, -1, 13);
 
             const __m128i MASK_4 = _mm_setr_epi8(
                 7, -1, -1, 8,
-                -1, 9, -1, -1, 10, 
-                -1, 11, -1, 12,
-                -1, 13, -1);
+                -1, 9, -1, -1,
+                10, -1, 11, -1,
+                12, -1, 13, -1);
 
             const __m128i bind = _mm_set_epi8(0, 0, '-', 0, 0, 0, 0, '-', 0, 0, 0, 0, 0, 0, 0, 0);
             const __m128i mask1 = _mm_shuffle_epi8(lower, MASK_1);
@@ -225,22 +194,21 @@ namespace meyr {
             __m128i lowerSorted = _mm_or_si128(mask3, mask4);
             lowerSorted = _mm_or_si128(lowerSorted, bind2);
 
-            ALIGNED_(16) char result[36];
-            _mm_store_si128(reinterpret_cast<__m128i *>(result), upperSorted);
-            _mm_store_si128(reinterpret_cast<__m128i *>(result + 16), lowerSorted);
+            ALIGNED_(16) std::array<char, 36> result;
+            _mm_store_si128(reinterpret_cast<__m128i *>(result.data()), upperSorted);
+            _mm_store_si128(reinterpret_cast<__m128i *>(result.data() + 16), lowerSorted);
 
-            const uint16_t v1 = _mm_extract_epi16(upper, 4);
-            const uint16_t v2 = _mm_extract_epi16(lower, 4);
-            // meh...
+            // Did not fit the last four chars. Extract and append them.
+            const int v1 = _mm_extract_epi16(upper, 7);
+            const int v2 = _mm_extract_epi16(lower, 7);
             result[32] = v1 & 0xff;
             result[33] = v2 & 0xff;
             result[34] = (v1 >> 8) & 0xff;
             result[35] = (v2 >> 8) & 0xff;
-            return std::string(result, 36);
+            return result;
         }
 
     private:
-
         template<HexCaseType hexCase = HEX_CASE_NONE, ValidationType validation = VALIDATE_ALL>
         int innerParse(const char* ptr)
         {
@@ -328,18 +296,9 @@ namespace meyr {
             hi = _mm_slli_epi16(hi, 4);
             const __m128i lo = _mm_sub_epi8(mask2, zero);
 
-            _mm_store_si128(reinterpret_cast<__m128i *>(_uuid.data), _mm_xor_si128(hi, lo));
+            _mm_store_si128(reinterpret_cast<__m128i *>(_data), _mm_xor_si128(hi, lo));
             return 0;
         }
-
-        union alignas(16) uuid
-        {
-            unsigned char data[16];
-            uint32_t uints[4];
-            uint64_t ulongs[2];
-        };
-
-        alignas(128) uuid _uuid;
 
         /*
         * Helper method for loading an aligned 16 byte repeated sse val
@@ -348,7 +307,7 @@ namespace meyr {
         static __m128i loadSimdCharArray(const char& chr)
         {
             ALIGNED_(16) const char y[17] = { chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr, chr };
-            return _mm_load_si128(reinterpret_cast<const __m128i*>(&y));
+            return _mm_load_si128(reinterpret_cast<const __m128i*>(y));
         }
 
         /*
@@ -359,88 +318,21 @@ namespace meyr {
         auto static loadAllowedCharRange() -> typename std::enable_if<hexCase == HEX_CASE_UPPER, __m128i>::type
         {
             ALIGNED_(16) const char _charRanges[17] = { '0', '9', 'A', 'Z', 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0, -1 };
-            return _mm_load_si128(reinterpret_cast<const __m128i*>(&_charRanges));
+            return _mm_load_si128(reinterpret_cast<const __m128i*>(_charRanges));
         }
         template<HexCaseType hexCase>
         auto static loadAllowedCharRange() -> typename std::enable_if<hexCase == HEX_CASE_LOWER, __m128i>::type
         {
             ALIGNED_(16) const char _charRanges[17] = { '0', '9', 'a', 'z', 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0, -1 };
-            return _mm_load_si128(reinterpret_cast<const __m128i*>(&_charRanges));
+            return _mm_load_si128(reinterpret_cast<const __m128i*>(_charRanges));
         }
         template<HexCaseType hexCase>
         auto static loadAllowedCharRange() -> typename std::enable_if<hexCase == HEX_CASE_NONE, __m128i>::type
         {
             ALIGNED_(16) const char _charRanges[17] = { '0', '9', 'A', 'Z', 'a', 'z', 0, -1, 0, -1, 0, -1, 0, -1, 0, -1 };
-            return _mm_load_si128(reinterpret_cast<const __m128i*>(&_charRanges));
+            return _mm_load_si128(reinterpret_cast<const __m128i*>(_charRanges));
         }
 
-        friend class UUIDGenerator;
-    };
-
-    class UUIDGenerator
-    {
-    public:
-        UUIDGenerator()
-        {
-            // if windows...
-            HCRYPTPROV random;
-            if (CryptAcquireContext(
-                &random,
-                nullptr,
-                nullptr,
-                PROV_RSA_FULL,
-                0))
-            {
-                ALIGNED_(16) unsigned char state[32];
-                CryptGenRandom(random, sizeof(state), state);
-
-                seed1 = _mm_load_si128(reinterpret_cast<const __m128i*>(&state[0]));
-                seed2 = _mm_load_si128(reinterpret_cast<const __m128i*>(&state[16]));
-
-                CryptReleaseContext(random, 0);
-            }
-        }
-
-        UUID operator()()
-        {
-            UUID newUuid;
-            const __m128i mask = loadSimdIntArray(0x0000FFFF);
-            const __m128i m1 = loadSimdIntArray(0x4650);
-            const __m128i m2 = loadSimdIntArray(0x78B7);
-            
-            __m128i amask = _mm_and_si128(seed1, mask);
-            __m128i ashift = _mm_srli_epi32(seed1, 0x10);
-            __m128i amul = _mm_mullo_epi32(amask, m1);
-            __m128i anew = _mm_add_epi32(amul, ashift);
-            seed1 = anew;
-
-            __m128i bmask = _mm_and_si128(seed2, mask);
-            __m128i bshift = _mm_srli_epi32(seed2, 0x10);
-            __m128i bmul = _mm_mullo_epi32(bmask, m2);
-            __m128i bnew = _mm_add_epi32(bmul, bshift);
-            seed2 = bnew;
-
-            __m128i bmasknew = _mm_and_si128(bnew, mask);
-            __m128i ashiftnew = _mm_slli_epi32(anew, 0x10);
-
-            __m128i result = _mm_or_si128(ashiftnew, bmasknew);
-            _mm_store_si128(reinterpret_cast<__m128i *>(newUuid._uuid.data), result);
-
-            *(&newUuid._uuid.data[6]) &= 0x4F;
-            *(&newUuid._uuid.data[6]) |= 0x40;
-
-            *(&newUuid._uuid.data[8]) &= 0xBF;
-            *(&newUuid._uuid.data[8]) |= 0x80;
-            return newUuid;
-        }
-
-    private:
-        __m128i seed1, seed2;
-
-        static __m128i loadSimdIntArray(const int& val)
-        {
-            ALIGNED_(16) const int y[4] = { val, val, val, val };
-            return _mm_load_si128(reinterpret_cast<const __m128i*>(&y));
-        }
+        ALIGNED_(16) unsigned char _data[16];
     };
 }
