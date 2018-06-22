36be057a817575be4af8e5c544d1861517cc4df3
yesmey <jesper.meyr@gmail.com>
2018-06-22 13:19:50 +0200

    Added some simple benchmark

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 SIMD implemented UUID parsing (stored with big endianness).
 
 Requires SSE4.2
+
+
+-----------
+
+Some benchmark numbers:
+
+Run on (8 X 3998 MHz CPU s)
+CPU Caches:
+  L1 Data 32K (x4)
+  L1 Instruction 32K (x4)
+  L2 Unified 262K (x4)
+  L3 Unified 8388K (x1)
+------------------------------------------------------------
+Benchmark                     Time           CPU Iterations
+------------------------------------------------------------
+BM_MyUUID                     8 ns          8 ns   89600000
+BM_WindowsUUID               46 ns         46 ns   15448276
+BM_BoostUUID                230 ns        230 ns    2986667
+BM_MyUUIDToString            70 ns         70 ns    8960000
+BM_BoostUUIDToString        130 ns        131 ns    5600000
+Press any key to continue . . .
--- main.cpp
+++ main.cpp
@@ -1,11 +1,67 @@
+#include <benchmark/benchmark.h>
 #include "UUID.h"
-#include <string>
-#include <iostream>
+#include <Windows.h>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/string_generator.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/uuid/uuid_io.hpp>
 
-int main(int arg, char** args)
-{
-    meyr::UUID guid;
+#define BOOST_UUID_USE_SSE41 1
 
-    if (guid.parse("{bebf06de-da19-4f71-b621-cba8865f5a06}"))
-        std::cout << guid.to_string().data() << std::endl;
+static void BM_MyUUID(benchmark::State& state) {
+	std::string x = "256a4180-65aa-42ec-a945-5fd21dec0538";
+	for (auto _ : state)
+	{
+		meyr::UUID guid;
+		guid.parse(x);
+	}
 }
+BENCHMARK(BM_MyUUID);
+
+static void BM_WindowsUUID(benchmark::State& state) {
+	auto x = L"{256a4180-65aa-42ec-a945-5fd21dec0538}";
+	for (auto _ : state)
+	{
+		GUID guid;
+		CLSIDFromString(x, (LPCLSID)&guid);
+	}
+}
+BENCHMARK(BM_WindowsUUID);
+
+static void BM_BoostUUID(benchmark::State& state) {
+	std::string x = "{256a4180-65aa-42ec-a945-5fd21dec0538}";
+	for (auto _ : state)
+	{
+		boost::uuids::string_generator gen;
+		boost::uuids::uuid u1 = gen(x);
+	}
+}
+BENCHMARK(BM_BoostUUID);
+
+static void BM_MyUUIDToString(benchmark::State& state) {
+	std::string x = "256a4180-65aa-42ec-a945-5fd21dec0538";
+	meyr::UUID guid;
+	guid.parse(x);
+
+	for (auto _ : state)
+	{
+		std::string s1 = guid.to_string('D');
+	}
+}
+BENCHMARK(BM_MyUUIDToString);
+
+
+static void BM_BoostUUIDToString(benchmark::State& state) {
+	std::string x = "{256a4180-65aa-42ec-a945-5fd21dec0538}";
+	boost::uuids::string_generator gen;
+	boost::uuids::uuid u1 = gen(x);
+
+	for (auto _ : state)
+	{
+		std::string s1 = boost::uuids::to_string(u1);
+	}
+}
+BENCHMARK(BM_BoostUUIDToString);
+
+BENCHMARK_MAIN();
+
